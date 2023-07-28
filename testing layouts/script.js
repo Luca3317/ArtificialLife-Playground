ac0a33dee58458a1978951c9badd5a0434851a30
Luca3317 <lweist3317@gmail.com>
2023-07-28 17:22:16 +0200

    Added header and fixed canvas resize (mostly)

--- testing layouts/script.js
+++ testing layouts/script.js
@@ -2,6 +2,7 @@ const resizer = document.querySelector("#resizer");
 const sidebar = document.querySelector("#sidebar");
 
 resizer.addEventListener("mousedown", (event) => {
+    event.preventDefault();
     $('html').css('cursor', 'col-resize');
     document.addEventListener("mousemove", resize, false);
     document.addEventListener("mouseup", () => {
@@ -14,7 +15,7 @@ function resize(e) {
     const size = `${window.innerWidth - e.x}px`;
     sidebar.style.flexBasis = size;
     fitContainer(cc[0]);
-    drawCircle(cc[0]);
+    drawGrid(cc[0]);
 }
 
 /** 
@@ -43,20 +44,32 @@ document.querySelector("#remove-content")
 
 const main = $("#main");
 const cc = $("#main-canvas");
-console.log(main);
-console.log(cc);
 const ctx = cc[0].getContext("2d");
-fitContainer(cc[0]);
-drawCircle(cc[0]);
+var xOffset = 0;
+var yOffset = 0;
+var cellSize = 40;
+var zoom = 1;
 
+fitContainer(cc[0]);
+drawGrid(cc[0]);
+
+$(window).on("resize", resize);
+
+// TODO (LIKELY DONE)
+// resizing can fuck up sometimes (might fix itself by adding on window resize resizing)
+// happens when zooming out (=> canvas gets longer) and going back to more normal zoom (=> container is higher because of
+// canvas overflow => canvas wont get set to smaller)
 function fitContainer(canvas) {
 
     var l = main.width() - parseFloat($("#main-canvas").css("marginLeft")) - parseFloat($("#main-canvas").css("marginRight"));
     console.log(l);
 
+    var height = clamp(main.height(), 0, window.innerHeight - $("header").height());
+    // TODO Likely some width equivalent for this too - at least right margin / width gets weird when zooming out / in
+
     // Make it visually fill the positioned parent
     canvas.width = main.width() - parseFloat(cc.css("marginLeft")) - parseFloat(cc.css("marginRight"));
-    canvas.height = main.height() - parseFloat(cc.css("marginTop")) - parseFloat(cc.css("marginBottom"));
+    canvas.height = height - parseFloat(cc.css("marginTop")) - parseFloat(cc.css("marginBottom"));
 }
 
 
@@ -64,4 +77,32 @@ function drawCircle(canvas) {
     ctx.beginPath();
     ctx.arc(100, 75, 50, 0, 2 * Math.PI);
     ctx.stroke();
+}
+
+function drawGrid(canvas) {
+    ctx.beginPath();
+
+    var useXOffset = xOffset % cellSize;
+    var useYOffset = yOffset % cellSize;
+
+    for (var i = -cellSize; i <= canvas.width + cellSize; i += cellSize * zoom) {
+        ctx.moveTo(useXOffset + i, 0);
+        ctx.lineTo(useXOffset + i, canvas.height);
+    }
+
+    for (var i = -cellSize; i <= canvas.height + cellSize; i += cellSize * zoom) {
+        ctx.moveTo(0, useYOffset + i);
+        ctx.lineTo(canvas.width, useYOffset + i);
+    }
+
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+}
+
+function clamp(value, min, max) {
+    if (min > max) throw new Error("Min can not be larger than max");
+    console.log("clamping " + value + " btween " + min + " and " + max);
+    if (value > max) value = max;
+    if (value < min) value = min;
+    return value;
 }
